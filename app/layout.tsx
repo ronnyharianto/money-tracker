72c023e053a57e2a01460cdbf1758109dd6c3e48
ronny <v.ronny.harianto@gmail.com>
2024-10-16 16:09:43 +0700

    install mui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,8 @@
 import type { Metadata } from "next";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
+import { Roboto } from 'next/font/google';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '@/themes/theme';
 import "@/styles/globals.css";
 
 export const metadata: Metadata = {
@@ -6,15 +10,26 @@ export const metadata: Metadata = {
   description: "A personal finance application",
 };
 
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`dark antialiased`}>
-        {children}
+    <html lang="en" data-lt-installed="true">
+      <body className={roboto.variable}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
